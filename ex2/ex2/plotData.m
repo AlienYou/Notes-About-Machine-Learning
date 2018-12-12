332d723cd2e87fe00d32ad5a49725303aee7116a
Alien You <AlienYou1997@163.com>
2018-12-12 17:22:12 +0800

    finish logistic regression

--- ex2/ex2/plotData.m
+++ ex2/ex2/plotData.m
@@ -15,21 +15,21 @@ figure; hold on;
 % ====================== Ng's Method =========================
 
 % Find Indices of Positive and Negative Examples
-%pos = find(y==1); neg = find(y == 0);
+pos = find(y==1); neg = find(y == 0);
 % Plot Examples
-%plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
-%'MarkerSize', 7);
-%plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
-%'MarkerSize', 7);
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 1, ...
+'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
+'MarkerSize', 7);
 
 % ====================== My Method ===========================
-for i = 1 : size(X, 1)
-	if y(i) == 1
-		plot(X(i, 1), X(i, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 5);
-	else
-		plot(X(i, 1), X(i, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 5);
-	end
-end
+%for i = 1 : size(X, 1)
+%	if y(i) == 1
+%		plot(X(i, 1), X(i, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 5);
+%	else
+%		plot(X(i, 1), X(i, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 5);
+%	end
+%end
 
 
 
